20e497e3aa5a4e607b55ad1bcc3699e992f8c10b
jstar_00 <wltjd6300@naver.com>
2025-11-16 10:01:55 +0900

    feat: btnIcn, checkbox SVGR 적용

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,22 @@
+// btnIcn
+export { default as Chevron_Down_Default } from '@svg/btnIcn/chevron_down_default.svg?react';
+export { default as Chevron_Down_Hover } from '@svg/btnIcn/chevron_down_hover.svg?react';
+export { default as Chevron_Down_Pressed } from '@svg/btnIcn/chevron_down_pressed.svg?react';
+export { default as Chevron_Up_Default } from '@svg/btnIcn/chevron_up_default.svg?react';
+export { default as Chevron_Up_Hover } from '@svg/btnIcn/chevron_up_hover.svg?react';
+export { default as Chevron_Up_Pressed } from '@svg/btnIcn/chevron_up_pressed.svg?react';
+export { default as Copy_Default } from '@svg/btnIcn/copy_default.svg?react';
+export { default as Copy_Hover } from '@svg/btnIcn/copy_hover.svg?react';
+export { default as Copy_Pressed } from '@svg/btnIcn/copy_pressed.svg?react';
+export { default as Folder_Default } from '@svg/btnIcn/folder_default.svg?react';
+export { default as Folder_Hover } from '@svg/btnIcn/folder_hover.svg?react';
+export { default as Folder_Pressed } from '@svg/btnIcn/folder_pressed.svg?react';
+export { default as Meatball_Default } from '@svg/btnIcn/meatball_default.svg?react';
+export { default as Meatball_Hover } from '@svg/btnIcn/meatball_hover.svg?react';
+export { default as Meatball_Pressed } from '@svg/btnIcn/meatball_pressed.svg?react';
+
+// checkbox
+export { default as Checkbox_Default } from '@svg/checkbox/checkbox_default.svg?react';
+export { default as Checkbox_Hover } from '@svg/checkbox/checkbox_hover.svg?react';
+export { default as Checkbox_Pressed } from '@svg/checkbox/checkbox_pressed.svg?react';
+export { default as Checkbox_Selected } from '@svg/checkbox/checkbox_selected.svg?react';
