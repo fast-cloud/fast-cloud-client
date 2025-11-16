a80dfd12d816abb78520d9f23166b9693ee88fff
jstar_00 <wltjd6300@naver.com>
2025-11-16 09:21:34 +0900

    fix: svgrOptions 수정(exportType 제거)

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
     vanillaExtractPlugin(),
     svgr({
       svgrOptions: {
-        exportType: 'named', // SVG를 컴포넌트 이름으로 import
         ref: true, // JavaScript로 SVG에 직접 접근 가능(useRef 등)
         svgo: false, // 원본 SVG 파일 그대로 import, 최적화 비활성화
         titleProp: true, // SVG에 <Logo title="" />과 같이 title 추가 가능(접근성 증대)
