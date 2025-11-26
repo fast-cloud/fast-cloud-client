25dd1c43ad86cd4372a10bdaa1cb1e8105360b4d
jstar_00 <wltjd6300@naver.com>
2025-11-26 19:00:22 +0900

    feat: RouterProvider 주입

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import 'pretendard/dist/web/variable/pretendardvariable.css';
 import './shared/styles/reset.css.ts';
 import './shared/styles/global.css.ts';
