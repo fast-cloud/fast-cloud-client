39052a9b9cc09a7d99cdc6b6aea6f4afbb5f6beb
jstar_00 <wltjd6300@naver.com>
2025-11-26 16:53:05 +0900

    feat: 헤더 고정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { TestPage } from '@/pages/Test/TestPage';
-import Home from './pages/home/Home';
 import Header from '@/shared/components/header/Header';
 
 function App() {
--- src/shared/components/header/Header.css.ts
+++ src/shared/components/header/Header.css.ts
@@ -1,10 +1,14 @@
 import { style } from '@vanilla-extract/css';
 
 export const container = style({
+  position: 'fixed',
+  top: 0,
+  left: 0,
   width: '100%',
   padding: '24px 60px',
   display: 'flex',
   justifyContent: 'space-between',
+  zIndex: 1000,
 });
 
 export const buttonContainer = style({
