e701ea3aca04bd0ff129f4c9e1bff6838ba4d4a5
jstar_00 <wltjd6300@naver.com>
2025-11-30 00:07:20 +0900

    init: tanstack-query 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import App from './App';
 import 'pretendard/dist/web/variable/pretendardvariable.css';
 import './shared/styles/reset.css.ts';
 import './shared/styles/global.css.ts';
+import QueryProvider from './apis/query-client.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryProvider>
+      <App />
+    </QueryProvider>
   </StrictMode>
 );
